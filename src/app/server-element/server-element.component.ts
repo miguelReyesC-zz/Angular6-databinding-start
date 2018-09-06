fc57c1ad52be11d10925ad1b1fbd63cc9ba8de85
Miguel <miguel_reyes_c@hotmail.com>
2018-09-06 20:28:16 -0300

    Getting Access to ng-content with @ContentChild

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, ContentChild, ElementRef, AfterContentInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,13 +6,22 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated // Emulated is the default value, others are none and native
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, AfterContentInit {
 
   @Input('srvElement') element: { type: string, name: string, content: string};
+
+  @ContentChild('contentParagraph') paragraph: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+    console.log('ngOnInit called!');
+    console.log('Text content of paragraph: ' + this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called!');
+    console.log('Text content of paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
 }
